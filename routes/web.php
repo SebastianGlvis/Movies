b8e2b4e80e80e6dd07b29d9f59e7d3462ada8d1e
JuanSe_05 <jsgalvis23@misena.edu.co>
2020-07-31 16:01:41 -0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/movie', 'MovieController');
+Route::get('movie/destroy/{id}', ['as' => 'movie/destroy', 'uses' => 'MovieControlller@destroy']);
+Route::post('movie/show', ['as' => 'movie/show', 'uses' => 'MovieController@show']);
+Route::post('movie/update/{id}', ['as' => 'movie/update', 'uses' => 'MovieController@update']);
+
+
