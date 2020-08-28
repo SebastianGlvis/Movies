fc7707fcd6961afc06e454f30bd2ac8d9262c6c6
JuanSe_05 <jsgalvis23@misena.edu.co>
2020-08-27 20:07:52 -0500

    roles

---
+++ database/migrations/2020_08_27_181351_create_status_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status', function (Blueprint $table) {
+            
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->unsignedBigInteger('type_status_id');
+
+            $table->foreign('type_status_id')->references('id')->on('type_statuses');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status');
+    }
+}
--- resources/views/movies/update.blade.php
+++ resources/views/movies/update.blade.php
@@ -5,6 +5,7 @@
             <article class="col-md-10 col-md-offset-1">
                 <form action="{{route('movie/update', ['id' => $movie->id])}}" method="post" novalidate>
                 @csrf
+                @method('PUT')
                     <div class="form-group">
                         <label>Nombre</label>
                         <input type="text" name="name" class="form-control" require value="{{$movie->name}}">
